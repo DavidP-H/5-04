3901ec8dcac376d0827a0f503f7c909698b7cf1c
DavidHart05 <>
2021-05-03 18:11:40 +0000

    format

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ document.getElementById('show').addEventListener('click', show)
 document.getElementById('hide').addEventListener('click', hide)
 
 function background () {
-  document.body.style.backgroundColor = "black"
+  document.body.style.backgroundColor = 'black'
 }
 
 function text () {
-  document.getElementById('paragraph').innerHTML = "This is some new text"
+  document.getElementById('paragraph').innerHTML = 'This is some new text'
 }
 
 function image () {
-  document.getElementById('picture').src = "/images/hedgehog.jpg"
+  document.getElementById('picture').src = '/images/hedgehog.jpg'
 }
 
 function hide () {
-document.getElementById('paragraph').style.display = "none"
+  document.getElementById('paragraph').style.display = "none"
 }
 
 function show () {
-document.getElementById('paragraph').style.display = "block"
+  document.getElementById('paragraph').style.display = "block"
 }
