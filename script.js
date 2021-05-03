70a5c52881fce85845d33e88dfc263e7a3652371
DavidHart05 <>
2021-05-03 17:54:53 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById('background').addEventListener('click', background)
+document.getElementById('text').addEventListener('click', text)
+document.getElementById('image').addEventListener('click', image)
+document.getElementById('show').addEventListener('click', show)
+document.getElementById('hide').addEventListener('click', hide)
+
+function background () {
+  document.body.style.backgroundColor = "black"
+}
+
+function text () {
+  document.getElementById('paragraph').innerHTML = "This is some new text"
+}
+
+function image () {
+  document.getElementById('picture').src = "/images/hedgehog.jpg"
+}
+
+function hide () {
+document.getElementById('paragraph').style.display = "none"
+}
+
+function show () {
+document.getElementById('paragraph').style.display = "block"
+}
